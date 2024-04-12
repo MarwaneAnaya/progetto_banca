2b42c699d8ab66378b078b8d4cd193a130f2af7f
Marwane <marwananaya43@gmail.com>
2024-04-12 09:40:21 +0200

    .

---
+++ Anaya_4BINT/libreria_laini/src/main/java/com/mycompany/libreria_laini/Libreria_laini.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.libreria_laini;
+
+/**
+ *
+ * @author Studente
+ */
+public class Libreria_laini {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
